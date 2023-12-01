9c7f97482876432e1501e84fb507d7d165bcdff0
d.alves <d.alves@unlimint.com>
2023-12-01 14:54:30 -0300

    Refactor ArrayList class

--- src/main/java/br/com/kualit/ArrayList.java
+++ src/main/java/br/com/kualit/ArrayList.java
@@ -5,37 +5,36 @@ import java.util.Objects;
 public class ArrayList<T> {
     private static final int TOTAL_SIZE = 10;
     private int size;
-    private Object[] elements;
+    private Object[] itens;
     public ArrayList(){
-        elements = new Object[TOTAL_SIZE];
+        itens = new Object[TOTAL_SIZE];
     }
     public int getSize(){
         return this.size;
     }
-    public void addItem(T element){
-        if(size >= elements.length)
+    public void addItem(T item){
+        if(size >= itens.length)
             increeseCapacity();
-        elements[size] = element;
-        size++;
+        itens[size++] = item;
     }
     private void increeseCapacity() {
-        int newCapacity  = elements.length * 2;
-        elements = Arrays.copyOf(elements, newCapacity);
+        int newCapacity  = itens.length * 2;
+        itens = Arrays.copyOf(itens, newCapacity);
     }
     public T getItemByIndex(int index){
-        if((index < 0 || index > elements.length) || Objects.isNull((T) elements[index]))
-            throw new ArrayIndexOutOfBoundsException("There is no element at this index. Invalid Index");
-        return (T) elements[index];
+        if((index < 0 || index > itens.length) || Objects.isNull((T) itens[index]))
+            throw new ArrayIndexOutOfBoundsException("There is no item at this index. Invalid Index");
+        return (T) itens[index];
     }
-    public void printElements() {
-        for(Object element : elements){
-            if(Objects.nonNull(element))
-                System.out.println(element);
+    public void printAllItems() {
+        for(Object item : itens){
+            if(Objects.nonNull(item))
+                System.out.println(item);
         }
     }
     public T removeItem(int index) {
-        T element = (T) elements[index];
-        elements[index] = null;
-        return element;
+        T item = (T) itens[index];
+        itens[index] = null;
+        return item;
     }
 }
